ac6679e3a4c58381487e713afaf8e8a3acbb9887
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-09-06 16:30:12 -0500

    changes to feature branch

--- main.cpp
+++ main.cpp
@@ -14,6 +14,16 @@ int sum(int n)
   return t; 
 }
 
+int product(int n)
+{
+  int t = 1;
+  for(int i = 1; i <= n; i++)
+    {
+      t = t * i;
+    }
+  return t; 
+}
+
 int main() {
   cout << "Hello World!" << endl; 
 
@@ -22,7 +32,7 @@ int main() {
   cin >> n;
   
   cout << "The value of the number added together from 1 to " << n << " is " << sum(n);
-  
+  cout << "The product of the number 1 to " << n << " is " << product(n);
   
   return 0;
 }
