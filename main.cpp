6afca102d797b1b7b53ef86b143dba0c519b5069
Justinfinn2891 <jfinn5@ut.utm.edu>
2023-09-06 16:10:02 -0500

    added function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,26 @@
 
 using namespace std;
 
+int sum(int n)
+{
+
+  int t = 0; 
+  for(int i = 0; i <= n; i++)
+    {
+      t += i; 
+    }
+  return t; 
+}
+
 int main() {
   cout << "Hello World!" << endl; 
+
+  int n;
+  cout << "Give a number: ";
+  cin >> n;
+  
+  cout << "The value of the number added together from 1 to " << n << " is " << sum(n);
+  
   
   return 0;
 }
